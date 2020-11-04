928d08ae3747bf1a5f77ee2e283691dd2cc99d8e
Alexei <62289011+never-m1nd@users.noreply.github.com>
2020-11-04 17:50:13 +0300

    Update forms.py

--- blogengine/blog/forms.py
+++ blogengine/blog/forms.py
@@ -22,30 +22,3 @@ class CreatePostForm(forms.Form):
 
         return new_post
 
-#
-# class CreatePostForm(forms.ModelForm):
-#     class Meta:
-#         model = Post
-#         fields = ['title', 'body', 'tags']
-
-    # widgets = {
-    #     'tags': forms.SelectMultiple()
-    # }
-
-    # def clean_slug(self):
-    #     new_slug = self.cleaned_data['slug']
-    #     return new_slug
-
-    # def save(self):
-    #     new_post = Post.objects.create(title=self.cleaned_data['title'],
-    #                                    body=self.cleaned_data['body'],)
-    #     for tag in self.cleaned_data['tags'].split(','):
-    #         if tag:
-    #             try:
-    #                 new_tag = Tag.objects.get(title=tag)
-    #             except ObjectDoesNotExist:
-    #                 new_tag = Tag.objects.create(title=tag)
-    #                 new_post.tags.add(new_tag)
-    #             else:
-    #                 new_post.tags.add(new_tag)
-    #     return new_post
